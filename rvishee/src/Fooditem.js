0ef8aac0d571ef2778cb8baaf70a957571d333dc
saharrrrr <skmao23@gmail.com>
2024-10-17 00:20:57 +0300

    add fooditem component

---
+++ rvishee/src/Fooditem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FoodItem = ({ name, price, rating, addToCart }) => {
+    return (
+        <div className="col-lg-3 col-md-6 mb-lg-0 mb-5">
+            <div className="card">
+                <img src={`assets/images/${name}.jpg`} className="img-fluid" alt="" loading="lazy" />
+                <div className="pt-5">
+                    <h4>{name}</h4>
+                    <p>Time: 15 - 20 Minutes <button className="btn btn-success">{rating} &#9733;</button></p>
+                    <span>&#36;{price}</span>
+                    <button className="mt-4 main-btn" onClick={() => addToCart(name, price)}>Order Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodItem;
