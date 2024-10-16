80a510c8edcd0ebd259375cb584a214e7aeaed1c
saharrrrr <skmao23@gmail.com>
2024-10-17 00:23:14 +0300

    add food data component

---
+++ rvishee/src/FoodData.js
@@ -0,0 +1,38 @@
+const foodsData = [
+    {
+        id: 1,
+        name: "Vegetable Sandwich",
+        price: 10,
+        rating: 4.3,
+        image: "assets/images/img-3.jpg"
+    },
+    {
+        id: 2,
+        name: "Vishee Burger",
+        price: 9,
+        rating: 3.9,
+        image: "assets/images/dosa.jpg"
+    },
+    {
+        id: 3,
+        name: "Beef Steak",
+        price: 14,
+        rating: 5.0,
+        image: "assets/images/idli.jpg"
+    },
+    {
+        id: 4,
+        name: "Raspberry Stuffed French Toast",
+        price: 10,
+        rating: 5.0,
+        image: "assets/images/img-5.jpg"
+    },
+    {
+        id: 5,
+        name: "Noodles",
+        price: 12,
+        rating: 3.5,
+        image: "assets/images/noodles.jpg"
+    }
+];
+export default foodsData;
